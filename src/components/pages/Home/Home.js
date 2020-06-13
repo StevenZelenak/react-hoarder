9ca267c0a43e88dfb5b8f5141e21e765ab9e41ea
Steven Zelenak <stevenmzelenak@gmail.com>
2020-06-13 10:40:40 -0500

    deploy done

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -4,10 +4,8 @@ import './Home.scss';
 class Home extends React.Component {
   render() {
     return (
-      <div className="Home">
+      <div className="Home mx-auto">
         <h1>Home</h1>
-
-
       </div>
     );
   }
